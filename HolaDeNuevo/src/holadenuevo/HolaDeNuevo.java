9b54d5f262902b099b1eec7279fa163bf8abdd0d
LaPlant12 <david.garcia.leon.12@gmail.com>
2020-08-29 13:47:19 -0500

    primer commit

---
+++ HolaDeNuevo/src/holadenuevo/HolaDeNuevo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holadenuevo;
+
+/**
+ *
+ * @author laplant
+ */
+public class HolaDeNuevo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i  = 7;
+        System.out.println("caca :  "+7);
+    }
+    
+}
